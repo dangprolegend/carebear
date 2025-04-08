6b143c81c314a08d7dda41c1d16f1812ee6206fa
Dang Nguyen <65774904+dangprolegend@users.noreply.github.com>
2025-04-07 23:54:54 -0400

    fix warning

--- backend/app.js
+++ backend/app.js
@@ -6,10 +6,7 @@ const app = express();
 app.use(express.json());
 
 // DB Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected'))
 .catch((err) => console.error(err));
 
