636dd4a8345d1536186929e37878d4136728deca
Minh Le <lennminh@gmail.com>
2025-04-11 21:53:54 -0400

    refactored db connection and wrote group endpoints

--- backend/models/Member.js
+++ backend/models/Member.js
@@ -1,15 +1,21 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const taskSchema = new mongoose.Schema({
-  userID: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Creator
-  groupID: { type: mongoose.Schema.Types.ObjectId, ref: 'Group' },
-  assignedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  status: { type: String, enum: ['pending', 'in-progress', 'done'], default: 'pending' },
-  description: { type: String },
-  deadline: { type: Date },
-  priority: { type: String, enum: ['low', 'medium', 'high'], default: 'medium' }
+const memberSchema = new mongoose.Schema({
+  userID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  groupID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Group',
+    required: true
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ['admin', 'caregiver', 'carereceiver']}
 });
 
-const Task = mongoose.model('Task', taskSchema);
-export default Task;
+const Member = mongoose.model('Member', memberSchema);
+export default Member;
