2a4353b72a67349f2abd623d787c2627319190ee
Daho Mita <dahomita17062006@gmail.com>
2025-04-13 19:27:55 -0400

    fix environment incompatibility

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,13 +1,16 @@
-module.exports = function(api) {
-    api.cache(true);
-    return {
-      presets: ['babel-preset-expo'],
-      plugins: [
-        ['module:react-native-dotenv', {
-          moduleName: '@env',
-          path: '.env',
-        }],
-      ],
-    };
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo']
+    //  plugins: [
+    //   [
+    //     'module-resolver',
+    //     {
+    //       alias: {
+    //         '@': './', // lets you use `@/components`, `@/hooks`, etc.
+    //       },
+    //     },
+    //   ],
+    // ],
   };
-  
+};
