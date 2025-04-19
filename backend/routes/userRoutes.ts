d593090bebb64f51c606693a7c0f10878ad9e4d4
Minh Le <lennminh@gmail.com>
2025-04-19 19:11:32 -0400

    separated user and auth routes

--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -1,13 +1,8 @@
 import express, { Router } from 'express';
-import { getUser, getAllUsers, signup, login, updateUser, deleteUser } from '../controllers/userController';
+import { getUser, getAllUsers, updateUser, deleteUser } from '../controllers/userController';
 
 const router: Router = express.Router();
 
-// Auth routes
-router.post('/signup', signup);
-router.post('/login', login);
-
-// User routes
 router.get('/', getAllUsers); // New route to get all users
 router.get('/:id', getUser);  // Changed from :userID to :id
 router.put('/:id', updateUser);  // Changed from :userID to :id
