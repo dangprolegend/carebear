2b465b4c2c964e3f30022a42428a1df4dce5eab5
Minh Le <lennminh@gmail.com>
2025-04-19 20:16:32 -0400

    completed migration of task routes

--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -1,10 +1,11 @@
 import express, { Router } from 'express';
-import { getUser, updateUser, deleteUser } from '../controllers/userController';
+import { getUser, updateUser, deleteUser, getUserTasks } from '../controllers/userController';
 
 const router: Router = express.Router();
 
 router.get('/:userID', getUser);  
 router.put('/:userID', updateUser);  
 router.delete('/:userID', deleteUser);  
+router.get('/:userID/tasks', getUserTasks)
 
 export default router;
