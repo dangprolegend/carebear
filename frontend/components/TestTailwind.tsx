50b38be7aa344a8eb4f4135af8f03ea15c8d15ed
Daho Mita <dahomita17062006@gmail.com>
2025-04-24 16:42:12 -0400

    configure nativewind and rnrdoc for frontend, design basic frame for health-input page

---
+++ frontend/components/TestTailwind.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TestTailwind = () => {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      Tailwind CSS is working!
+    </div>
+  );
+};
+
+export default TestTailwind;
