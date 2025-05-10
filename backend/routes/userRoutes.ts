1b7ac40b73ce1cba39f945a7bc6c55c8c021163a
Minh Le <lennminh@gmail.com>
2025-05-09 21:54:19 -0400

    patch method to save additional info during onboarding

--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -1,10 +1,11 @@
 import express, { Router } from 'express';
-import { getUser, updateUser, deleteUser, getUserTasks, getUserNotifications } from '../controllers/userController';
+import { getUser, updateUser, deleteUser, getUserTasks, getUserNotifications, provideAdditionalUserInfo } from '../controllers/userController';
 
 const router: Router = express.Router();
 
 router.get('/:userID', getUser);  
 router.put('/:userID', updateUser);  
+router.patch('/:userID/onboarding', provideAdditionalUserInfo);
 router.delete('/:userID', deleteUser);  
 router.get('/:userID/tasks', getUserTasks);
 router.get('/:userID/notifications', getUserNotifications); 
