582981dea64202d7ad4ddd0273194c8fc4d429c6
Dang Nguyen <65774904+dangprolegend@users.noreply.github.com>
2025-05-13 10:41:49 -0400

    dashboard tasks fixed

--- frontend/app/(protected)/dashboard/mydashboard/dashboard.tsx
+++ frontend/app/(protected)/dashboard/mydashboard/dashboard.tsx
@@ -3,11 +3,9 @@ import DashboardBase from './DashboardBase';
 const Dashboard = () => {
   const tasks = [
     { time: '8:00 am', type: 'Breakfast', title: 'Medicine 1', detail: '1 Tablet', subDetail: 'after breakfast', checked: true },
-    { time: '8:00 am', type: 'Breakfast', title: 'Supplement 1', detail: '1 Tablet', subDetail: 'after breakfast' },
     { time: '12:00 pm', type: 'Lunch', title: 'Supplement 1', detail: '1 Tablet', subDetail: 'after lunch' },
     { time: '1:00 pm', type: 'Lunch', title: 'Help Grandm...', detail: 'Bedroom' },
     { time: '6:00 pm', type: 'Dinner', title: 'Medicine 2', detail: '1 Tablet', subDetail: 'after dinner' },
-    { time: '6:00 pm', type: 'Dinner', title: 'Supplement 2', detail: '1 Tablet', subDetail: 'after dinner' },
   ];
 
   return <DashboardBase tasks={tasks} />;
