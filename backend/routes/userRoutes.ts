34c53fc347bed0f3f614c9dd8aa8bcc2f0d5f406
Minh Le <lennminh@gmail.com>
2025-05-31 11:04:30 -0600

    added endpoints for email invitation and modified join group function

--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -1,6 +1,7 @@
 import express, { Router } from 'express';
 import { getUser, provideAdditionalUserInfo, getUserIdByClerkId, getUserGroup, getUserInfo, getFamilyMembers } from '../controllers/userController';
 import { createGroup, joinGroup } from '../controllers/groupController';
+import { sendInvitation } from '../controllers/invitationController';
 
 const router: Router = express.Router();
 
@@ -11,5 +12,6 @@ router.post('/:userID/createGroup', createGroup);
 router.patch('/:userID/joinGroup', joinGroup); 
 router.get('/:userID/info', getUserInfo);
 router.get('/:userID/familyMembers', getFamilyMembers);
+router.post('/:userID/invite', sendInvitation);
 
 export default router;
