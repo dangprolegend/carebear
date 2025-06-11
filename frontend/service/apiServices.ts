092f6fedfb5ad81b48362487dc22603792d50c76
Daho Mita <dahomita17062006@gmail.com>
2025-06-12 00:26:50 +0700

    save the day

--- frontend/service/apiServices.ts
+++ frontend/service/apiServices.ts
@@ -1,6 +1,6 @@
 import { Task as FrontendTaskType } from '../app/(protected)/dashboard/mydashboard/task';
 
-const API_BASE_URL = "https://fe1f-2402-800-6f5f-1fc4-e57d-a6e3-1747-a39c.ngrok-free.app";
+const API_BASE_URL = "https://23e1-2402-800-61ae-d326-180b-9fcd-c699-fdb9.ngrok-free.app";
 
 console.log("apiService.ts: Using API Base URL:", API_BASE_URL);
 
