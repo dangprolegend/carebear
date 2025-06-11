65062df1988afa771470b05a80365a672e64f18a
Daho Mita <dahomita17062006@gmail.com>
2025-06-12 00:38:52 +0700

    merge with master

--- backend/routes/dailyRoutes.ts
+++ backend/routes/dailyRoutes.ts
@@ -1,10 +1,9 @@
 import express, { Router } from 'express';
-import { checkTodayStatus, getStatusHistory, getStatusStats, getTodayStatus, submitDailyStatus, updateDailyStatus } from '../controllers/dailyController';
+import { checkTodayStatus, getStatusHistory, getStatusStats, getTodayStatus, submitDailyStatus } from '../controllers/dailyController';
 
 const router: Router = express.Router();
 
 router.post('/submit/:userID', submitDailyStatus);
-router.put('/update/:userID', updateDailyStatus); // new route to update mood and body
 router.get('/check/:userID', checkTodayStatus);
 router.get('/today/:userID', getTodayStatus);
 router.get('/history/:userID', getStatusHistory);
